85d1a3b62a8a424dfc95f84429e6f665e4abc09b
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2024-03-04 19:43:44 +0530

    passkey established

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-//Passport
+// Passport
 const PassportService = require("../app/services/passport-services");
 const SessionChallengeStore =
   require("passport-fido2-webauthn").SessionChallengeStore;
@@ -17,7 +17,19 @@ const auth = new (require("../app/controllers/auth"))();
 const admin = new (require("../app/controllers/admin"))();
 
 router.get("/", pages.welcome, admin.dashboard);
+
 router.get("/login", auth.login);
+router.post(
+  "/login/public-key",
+  auth.passportCheck(),
+  auth.admitUser,
+  auth.denyUser
+);
+router.post("/login/public-key/challenge", auth.getChallengeFrom(store));
+
+router.post("/logout", auth.logout);
+
 router.get("/register", auth.register);
+router.post("/register/public-key/challenge", auth.createChallengeFrom(store));
 
 module.exports = router;
