52c70bde1a2c469d9b8dd0ef830128119a710b1c
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2024-03-04 18:44:59 +0530

    session keys init

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = "0.0.0.0";
 
+const multer = require("multer");
+const cookieParser = require("cookie-parser");
+
+const passport = require("passport");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const path = require("path");
 const layouts = require("express-ejs-layouts");
+
+const db = require("./db/helpers/init");
+
+// Session store
+const sessionStore = new SequelizeStore({
+  db: db,
+});
+
+// JSON, form data and url encoding
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(multer().none());
+app.use(cookieParser());
 
 // Templates
 app.use(layouts);
@@ -14,6 +34,21 @@ app.set("view engine", "ejs");
 
 // Static files
 app.use(express.static(__dirname + "/public"));
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    },
+  })
+);
+sessionStore.sync();
+app.use(passport.authenticate("session"));
 
 //Routes
 app.use("/", require("./config/routes"));
