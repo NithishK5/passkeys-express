9923cbc48fe8ba0a9726d036d3e6f35a3e099690
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2024-03-04 13:45:36 +0530

    controller init

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = "0.0.0.0";
 
-app.get("/", (req, res) => {
-  res.send("Hello hzh!");
-});
+//Routes
+app.use("/", require("./config/routes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
